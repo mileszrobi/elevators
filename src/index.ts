7d07c9967b41f187a85170500442a6f87a88a93d
Rob <robert.milesz@codecool.com>
2023-07-17 11:09:50 +0200

    Calculating travel cost if elevator is idle.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,28 @@
-export function dummy(a: number): number {
-  return a * 2;
+export class Elevator {
+  totalCapacity: number;
+  currentCapacity: number;
+  workQueue: [number, number][]; // floor, delta capacity pairs
+  onFloor: number;
+
+  constructor(totalCapacity: number) {
+    this.totalCapacity = totalCapacity;
+    this.currentCapacity = totalCapacity;
+    this.workQueue = [];
+    this.onFloor = 0;
+  }
+
+  travelCost(from: number, to: number): number {
+    return Math.abs(from - this.onFloor) + Math.abs(to - from);
+  }
+}
+
+export class ElevatorController {
+  elevators: Elevator[];
+
+  constructor(elevatorCount: number, elevatorCapacity: number) {
+    this.elevators = [];
+    for (let i = 0; i < elevatorCount; i++) {
+      this.elevators[i] = new Elevator(elevatorCapacity);
+    }
+  }
 }
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,7 +1,26 @@
-import { dummy } from "../src";
+import { Elevator } from "../src";
 
-describe('dummy test', () => {
-  test('some test', () => {
-    expect(dummy(2)).toBe(4);
+let elevator: Elevator;
+
+describe('Travel cost for an elevator equals to the sum of getting to the "from" floor then to the "to" floor, if the elevator is idle', () => {
+  beforeEach(() => {
+    elevator = new Elevator(1);
+  });
+
+  test('moving one floor up from ground', () => {
+    expect(elevator.travelCost(0, 1)).toBe(1);
+  });
+
+  test('picking up the passenger on the way up', () => {
+    expect(elevator.travelCost(1, 2)).toBe(2);
+  });
+
+  test('passenger travels in the opposite direction', () => {
+    expect(elevator.travelCost(2, 1)).toBe(3);
+  });
+
+  test('consider current floor', () => {
+    elevator.onFloor = 5;
+    expect(elevator.travelCost(6, 4)).toBe(3);
   });
 });
